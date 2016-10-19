bf6e3217fbf598f21f881092360763660cb3f397
Tom Nunn <t@tomnunn.uk>
2016-10-19 17:13:25 +0100

    add password confirmation funtionality and modified create_user view

--- spec/features/creating_user_spec.rb
+++ spec/features/creating_user_spec.rb
@@ -1,8 +1,13 @@
 feature 'Creating users' do
 	scenario 'creating a user' do
-		create_user
+		create_user('ben@example.com', 'password', 'password')
 		initial_length = User.all.length
-		create_user
+		create_user('ben@example.com', 'password', 'password')
 		expect(User.all.length).to be > initial_length
+	end
+
+	scenario 'user provides mismatching passwords' do
+		create_user('ben@example.com', 'apples', 'pears')
+		expect(User.all.length).to be(0)
 	end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,7 +1,8 @@
-def create_user
+def create_user(email, password, password_confirmation)
 	visit('/users')
 
-	fill_in :email, with: "ben@example.com"
-	fill_in :password, with: "password"
+	fill_in :email, with: email
+	fill_in :password, with: password
+	fill_in :password_confirmation, with: password_confirmation
 	click_button('Sign Up')
 end

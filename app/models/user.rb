ce6cf2964726ed7498c636d1f93f9babe23ad062
Stephen Gregory <stejgregory@gmail.com>
2016-10-21 15:00:38 +0100

    Added prevention for duplicate email accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,13 @@ class User
 	attr_accessor :password_confirmation
 
 	property :id, Serial
-	property :email, String, required: true
+	property :email, String, required: true, format: :email_address, unique: true
 	property :password_digest, Text
 
-	validates_presence_of :email
+	# validates_presence_of :email
 	validates_confirmation_of :password
+	# validates_format_of :email as:email_address
+	# validates_uniqueness_of :email
 
 	def password=(password)
 		@password = password
--- spec/features/creating_user_spec.rb
+++ spec/features/creating_user_spec.rb
@@ -6,7 +6,7 @@ feature 'Creating users' do
 	scenario 'user provides mismatching passwords' do
 		create_user(password_confirmation: 'wrong')
   	expect(current_path).to eq('/users')
-  	expect(page).to have_content 'Password and confirmation password do not match'
+  	expect(page).to have_content 'Password does not match the confirmation'
 	end
 
 	scenario "I can't sign up without an email address" do
@@ -17,4 +17,9 @@ feature 'Creating users' do
     expect { create_user(email: "invalid@email") }.not_to change(User, :count)
   end
 
+	scenario "I can't sign up with an email address if it is already in use" do
+		create_user
+		expect { create_user }.not_to change(User, :count)
+		expect(page).to have_content('Email is already taken')
+	end
 end
